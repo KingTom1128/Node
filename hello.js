3307227ef8ac789e909e29596e45b0138ff9816e
jintao19981128 <974636731@qq.com>
2019-03-10 15:50:06 +0800

    Initial commit

---
+++ hello.js
@@ -0,0 +1,18 @@
+var http = require('http');
+http.createServer(function (request, response){
+  response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  if(request.url!=="/favicon.ico"){
+    //清楚第二次访问
+    console.log('访问');
+    response.write('Hello,World');
+    //不写则没有http协议尾，但写了会产生两次访问
+    response.end('hello, 世界');
+  }
+}).listen(8000);
+console.log('Server running at http://127.0.0.1:8000/');
+/**
+ * 启动服务
+ * cmd下执行
+ * node hello.js
+ * 浏览器访问：http://localhost:8000
+ */
